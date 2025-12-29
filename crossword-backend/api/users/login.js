e7d58a25b83b6de043912b3532bf44ff6e210daf
JoaoPMV <joaop.trilha@gmail.com>
2025-12-29 05:26:33 -0300

    Adiciona endpoint serverless /api/users/login

--- crossword-backend/api/users/login.js
+++ crossword-backend/api/users/login.js
@@ -1,8 +1,15 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    // Sua lógica de login aqui
-    res.status(200).json({ mensagem: "Login OK!" });
+    // Simula resposta de sucesso
+    res.status(200).json({
+      token: "faketoken123456789",
+      user: {
+        id: 1,
+        email: "teste@email.com",
+        nome: "Joao",
+      },
+    });
   } else {
-    res.status(405).json({ erro: "Método não permitido" });
+    res.status(405).json({ error: "Método não permitido" });
   }
 }
